80c2478102956c987d7aba3e44678e4a8f6cd95e
Lamine DIOP <diop.lamine3@ugb.edu.sn>
2019-08-29 08:03:06 +0000

    Update KAIS19.py

--- NUSSampling/KAIS19.py
+++ NUSSampling/KAIS19.py
@@ -1,5 +1,25 @@
 #-*- coding:Utf-8 -*-
 
+# NUSSampling
+# Copyright (c) 2019  Lamine Diop (1;2), Cheikh Talibouya Diop (1), Arnaud Giacometti (2), Dominique Li (2) and Arnaud Soulet (2)
+# (1) University of Gaston Berger of Saint-Louis, Senegal, Email: {diop.lamine3, cheikh-talibouya.diop}@ugb.edu.sn
+# (2) University of Tours, France, Email: {arnaud.giacometti, dominique.li, arnaud.soulet}@univ-tours.fr
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# ---------------------------------------------------------------------
+
 __author__ = "DIOP Lamine BSF"
 
 import os
